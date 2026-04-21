744c0e20f2a531e684e1b3880bd3d2db636cfb7a
Muhammed Hafis <muhammedhafis302@gmail.com>
2026-04-21 14:54:25 +0530

    about page deleted

--- app/(features)/about/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function About() {
-    return (
-        <div className="flex items-center justify-center min-h-[50vh]" data-aos="fade-up">
-            <h1 className="text-4xl font-bold text-brand-blue">About</h1>
-        </div>
-    );
-}       
