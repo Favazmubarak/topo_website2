8a741226377d6dd6c4cc7c4b4fddd6961d926e97
Muhammed Hafis <muhammedhafis302@gmail.com>
2026-04-29 18:52:22 +0530

    refresh token function removed

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -6,20 +6,6 @@ const axiosInstance = axios.create({
   withCredentials: true,
 });
 
-let isRefreshing = false;
-let failedQueue: any[] = [];
-
-const processQueue = (error: any, token: string | null = null) => {
-  failedQueue.forEach((prom) => {
-    if (error) {
-      prom.reject(error);
-    } else {
-      prom.resolve(token);
-    }
-  });
-  failedQueue = [];
-};
-
 
 axiosInstance.interceptors.request.use((config) => {
   const token = useAuthStore.getState().accessToken;
@@ -34,53 +20,13 @@ axiosInstance.interceptors.request.use((config) => {
 
 axiosInstance.interceptors.response.use(
   (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-
-
-    const skipRefresh = ["/auth/login", "/auth/refresh", "/auth/logout"].some(
-      (path) => originalRequest?.url?.includes(path)
-    );
-
-    if (error.response?.status === 401 && !originalRequest._retry && !skipRefresh) {
-      if (isRefreshing) {
-        return new Promise((resolve, reject) => {
-          failedQueue.push({ resolve, reject });
-        })
-          .then((token) => {
-            originalRequest.headers.Authorization = "Bearer " + token;
-            return axiosInstance(originalRequest);
-          })
-          .catch((err) => Promise.reject(err));
-      }
-
-      originalRequest._retry = true;
-      isRefreshing = true;
-
-      try {
-        const refreshResponse = await axiosInstance.post("/auth/refresh", {});
-        const { accessToken } = refreshResponse.data;
-
-        if (accessToken) {
-          useAuthStore.getState().setAccessToken(accessToken);
-          processQueue(null, accessToken);
-          originalRequest.headers.Authorization = `Bearer ${accessToken}`;
-          return axiosInstance(originalRequest);
-        }
-      } catch (refreshError) {
-        processQueue(refreshError, null);
-        useAuthStore.getState().logout();
-        if (typeof window !== "undefined") {
-          window.location.href = "/admin/login";
-        }
-        return Promise.reject(refreshError);
-      } finally {
-        isRefreshing = false;
-      }
+  (error) => {
+    if (error.response?.status === 401) {
+      useAuthStore.getState().logout();
     }
-
     return Promise.reject(error);
   }
 );
+
 
 export default axiosInstance;
